f4cd699bd75e4e2fd9c3aea057ad13c8a7dc5826
geunwoo <olo90632951@gmail.com>
2023-11-12 12:43:35 +0900

    feat: 주문 메뉴와 개수를 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import InputView from "./utils/InputView.js";
 import OutputView from "./utils/OutputView.js";
-import GiftManager from "./GiftManager.js";
-import BadgeManager from "./BadgeManager.js";
-import DiscountDateManager from "./DiscountDateManager.js";
-import DiscountManager from "./DiscountManager.js";
-import { DESSERT, MAIN, MENU } from "./constant/menu.js";
-import BENEFIT from "./constant/Benefit.js";
-import { Console } from "@woowacourse/mission-utils";
-import Orders from "./Orders.js";
 
 class App {
   constructor() {
@@ -16,6 +8,7 @@ class App {
 
   async run() {
     const inputDate = await InputView.readDate();    
+    const inputMenu = await InputView.readMenu();
   }
 }
 
