4ac19cabdf3303293ccc41047907dce5b43599bd
geunwoo <olo90632951@gmail.com>
2023-11-12 19:34:16 +0900

    refactor: Formatter 문자열 포맷  정적 메서드 추가

---
+++ src/utils/Formatter.js
@@ -0,0 +1,11 @@
+class Formatter {  
+  static splitComma(value) {
+    return value.split(',');    
+  }
+
+  static splitDash(value) {
+    return value.split('-');
+  }
+}
+
+export default Formatter;
