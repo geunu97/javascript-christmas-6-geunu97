8377d15568606175fe61e2a5aef3ec2fe08985e0
geunwoo <olo90632951@gmail.com>
2023-11-12 13:14:04 +0900

    feat: 방문날짜가 2023년 12월 중 일요일~목요일인지 계산하는 기능 추가

--- src/DiscountDateManager.js
+++ src/DiscountDateManager.js
@@ -5,12 +5,21 @@ class DiscountDateManager {
     this.#date = date;    
   }
 
+  #getDayOfWeek() {
+    return new Date(2023, 11, this.#date).getDay();
+  }
+
   getProfitDay() {
     return this.#date - 1;
   }
 
   isChristmasEvent() {
     return this.#date >= 1 && this.#date <= 25;
+  }
+
+  isWeekdayEvent() {
+    const dayOfWeek = this.#getDayOfWeek();
+    return dayOfWeek >= 0 && dayOfWeek <= 4;
   }
 }
 
