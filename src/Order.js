bf1a4636a21e4b6c1ff6786a500e612735e5cc25
geunwoo <olo90632951@gmail.com>
2023-11-12 13:56:05 +0900

    fix: 메뉴가 몇 개 포함되었는지 계산하는 기능 수정

--- src/Order.js
+++ src/Order.js
@@ -1,3 +1,4 @@
+import { MENU } from "./constant/menu.js";
 import OutputView from "./utils/OutputView.js";
 
 class Order {  
@@ -8,6 +9,17 @@ class Order {
     this.#menu = menu
     this.#count = count;
     OutputView.printMenu(menu, count);
+  }
+
+  getTotalPrice() {
+    return MENU[this.#menu] * this.#count;
+  }
+
+  getCountIncludedMenuType(menuType){
+    if (menuType.hasOwnProperty(this.#menu)) {
+      return this.#count;
+    }
+    return 0;
   }
 }
 
