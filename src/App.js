9abcb140292e511266dbb4f5a40bd365aac6c0c0
geunwoo <olo90632951@gmail.com>
2023-11-12 12:41:35 +0900

    feat: 12월의 방문 날짜를 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import InputView from "./utils/InputView.js";
+import OutputView from "./utils/OutputView.js";
+import GiftManager from "./GiftManager.js";
+import BadgeManager from "./BadgeManager.js";
+import DiscountDateManager from "./DiscountDateManager.js";
+import DiscountManager from "./DiscountManager.js";
+import { DESSERT, MAIN, MENU } from "./constant/menu.js";
+import BENEFIT from "./constant/Benefit.js";
+import { Console } from "@woowacourse/mission-utils";
+import Orders from "./Orders.js";
+
 class App {
-  async run() {}
+  constructor() {
+    OutputView.printEventTitle();
+  }
+
+  async run() {
+    const inputDate = await InputView.readDate();    
+  }
 }
 
 export default App;
